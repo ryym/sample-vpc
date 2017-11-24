92b34b0cd41d37f33fe7b9e40a9a8e7ab78a1d10
ryym <ryym.64@gmail.com>
2017-11-24 15:55:14 +0900

    Create IGW and route table

--- sample-vpc.tf
+++ sample-vpc.tf
@@ -48,3 +48,31 @@ resource "aws_subnet" "sample_db2" {
         Name = "sample-vpc2:private:db2"
     }
 }
+
+resource "aws_internet_gateway" "sample" {
+    vpc_id = "${aws_vpc.sample.id}"
+
+    tags {
+        Name = "igw-sample-vpc2"
+    }
+}
+
+# Route table
+
+resource "aws_route_table" "sample_public" {
+    vpc_id = "${aws_vpc.sample.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.sample.id}"
+    }
+
+    tags {
+        Name = "sample-vpc2:public"
+    }
+}
+
+resource "aws_route_table_association" "sample_web" {
+    subnet_id = "${aws_subnet.sample_web.id}"
+    route_table_id = "${aws_route_table.sample_public.id}"
+}
